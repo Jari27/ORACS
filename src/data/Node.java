d802613001604504742274feb008c00df285be09
Jari Meevis <jarimeevis@gmail.com>
2019-02-18 15:15:35 +0100

    Make node locaton public

--- src/data/Node.java
+++ src/data/Node.java
@@ -1,16 +1,6 @@
 package data;
 
 public class Node {
-	int x,y;
-	
-	public Node() {
-		this.x = -1;
-		this.y = -1;
-	}
-	
-	public Node(NodeType type, int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+	public int x, y;
 	
 }
